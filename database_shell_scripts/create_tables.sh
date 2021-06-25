69b8ccc2ca68c819f45131d04551a47b98f9d19e
jason-gill00 <59940873+jason-gill00@users.noreply.github.com>
2021-06-25 15:16:46 -0400

    Update create_tables.sh

--- database_shell_scripts/create_tables.sh
+++ database_shell_scripts/create_tables.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
-sqlplus64 "jcabugua/11245288@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<-EOF
+sqlplus64 "oracle_url" <<-EOF
 CREATE TABLE Person (
     BirthDate DATE,
     Email VARCHAR2(20) NULL,
