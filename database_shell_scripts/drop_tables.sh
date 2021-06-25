3ebe230cbd64f357b6ed1a6d6bf197656031599f
jason-gill00 <59940873+jason-gill00@users.noreply.github.com>
2021-06-25 15:17:11 -0400

    Update drop_tables.sh

--- database_shell_scripts/drop_tables.sh
+++ database_shell_scripts/drop_tables.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
-sqlplus64 "jcabugua/11245288@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<-EOF
+sqlplus64 "oracle_url" <<-EOF
   DROP TABLE BOOKS;
   DROP TABLE VIDEOS;
   DROP TABLE RENTALORDER;
